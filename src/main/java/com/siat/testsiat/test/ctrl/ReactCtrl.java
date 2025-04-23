df376f5351e8f716cb401827f329332572fd9838
chQ <gudrb019@naver.com>
2025-04-23 16:16:31 +0900

    update reactcontroller

--- src/main/java/com/siat/testsiat/test/ctrl/ReactCtrl.java
+++ src/main/java/com/siat/testsiat/test/ctrl/ReactCtrl.java
@@ -12,11 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 import com.siat.testsiat.test.model.dto.TodoRequestDTO;
 import com.siat.testsiat.test.model.dto.TodoResponseDTO;
 import com.siat.testsiat.test.service.TestService;
+import com.siat.testsiat.user.model.domain.UserRequestDTO;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @RestController
@@ -27,7 +31,7 @@ public class ReactCtrl {
     @Autowired
     private TestService service;
     
-    @GetMapping("/select")
+    @PostMapping("/select")
     public ResponseEntity<List<TodoResponseDTO>> select(@RequestBody TodoRequestDTO params) {
          System.out.println("debug > reactCtrl > select ");
 
@@ -37,6 +41,19 @@ public class ReactCtrl {
          
          return new ResponseEntity<List<TodoResponseDTO>>(list, HttpStatus.OK);
      }
-
+     @PostMapping("/login")
+     public ResponseEntity<Void> postMethodName(@RequestBody UserRequestDTO params) {
+         System.out.println("debug >> react ctrl / login");
+         System.out.println("devuf >> react ctrl /loing params : " + params);
+         // 204 == noContent
+         return ResponseEntity.noContent().build();
+     }
+    @GetMapping("/list")
+    public ResponseEntity<List<TodoResponseDTO>> select() {
+         System.out.println("debug > reactCtrl > list ");
+         List<TodoResponseDTO> list = service.selectService();
+         
+         return new ResponseEntity<List<TodoResponseDTO>>(list, HttpStatus.OK);
+     }
     
 }
